763eaafd7e165062eed2eb3e9399fc8e86029d05
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2024-01-20 17:25:03 -0700

    finished

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,47 @@
 
 /* Step 2 - Variables */
 
-
+let fullName = "Spencer Damiano";
+let currentYear = 2024;
+let profilePicture = "./images/me.JPG";
 
 
 /* Step 3 - Element Variables */
 
-
+let nameElement = document.getElementById("name");
+let foodElement = document.getElementById("food");
+let yearElement = document.querySelector("#year");
+let imgElement = document.querySelector("img");
 
 
 
 /* Step 4 - Adding Content */
 
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imgElement.setAttribute("src", profilePicture);
 
 
 
 
 /* Step 5 - Array */
+
+// step 5.1 - 5.3
+let myFavoriteFoods = ["Cheese Steak", "Hamburger", "French Fries", "Tacos", "Thai Red Curry"];
+foodElement.innerHTML = `${myFavoriteFoods}`;
+
+// step 5.4 - 5.5
+let myMostFavorite = "Medium Rare Stake";
+myFavoriteFoods.push(myMostFavorite);
+foodElement.innerHTML += `<br> ${myFavoriteFoods}`
+
+// step 5.6 - 5.7
+myFavoriteFoods.shift();
+foodElement.innerHTML += `<br> ${myFavoriteFoods}`;
+
+// step 5.8 - 5.9
+myFavoriteFoods.pop();
+foodElement.innerHTML += `<br> ${myFavoriteFoods}`;
 
 
 
